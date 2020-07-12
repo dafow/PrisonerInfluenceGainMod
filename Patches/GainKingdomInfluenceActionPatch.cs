ca39801e2facee459266ef76a6584f162bbb6bde
dafow <falah.salim@gmail.com>
2020-07-12 18:34:14 -0400

    mod template cleanup + implementation

---
+++ Patches/GainKingdomInfluenceActionPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+
+namespace PrisonerInfluenceGainMod.Patches
+{
+    [HarmonyPatch(typeof(GainKingdomInfluenceAction), "ApplyForLeavingTroopToGarrison")]
+    class GainKingdomInfluenceActionPatch
+    {
+        public static void DoPatching()
+        {
+            var harmony = new Harmony("mod.bannerlord.prisonerinfluencegain");
+            harmony.PatchAll();
+        }
+
+        static void Prefix(Hero hero, ref float value)
+        {
+            value *= PrisonerInfluenceGainModSubModule.Multiplier;
+        }
+    }
+}
---
+++ PrisonerInfluenceGainModSubModule.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using PrisonerInfluenceGainMod.Patches;
+using System.IO;
+using TaleWorlds.MountAndBlade;
+
+namespace PrisonerInfluenceGainMod
+{
+	public class PrisonerInfluenceGainModSubModule : MBSubModuleBase
+	{
+		public static float Multiplier { get; set; }
+
+		protected override void OnBeforeInitialModuleScreenSetAsRoot()
+		{
+			using (StreamReader file = File.OpenText("../../Modules/PrisonerInfluenceGainMod/bin/Win64_Shipping_Client/settings.json"))
+			{
+				var definition = new { Multiplier = 1 };
+				var settings = JsonConvert.DeserializeAnonymousType(file.ReadToEnd(), definition);
+				PrisonerInfluenceGainModSubModule.Multiplier = settings.Multiplier;
+			}
+			
+			GainKingdomInfluenceActionPatch.DoPatching();
+        }
+	}
+}
